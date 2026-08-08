9ad08717f612d13f53dc2a1c19b68a5e153a050b
viktorrox-prog <viktor.rox@yandex.ru>
2026-08-08 15:55:01 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,8 @@
 import type { NextConfig } from "next";
 
-const basePath =
-  process.env.BASE_PATH !== undefined ? process.env.BASE_PATH : "";
-
 const nextConfig: NextConfig = {
-  output: process.env.OUTPUT_MODE === "export" ? "export" : undefined,
-
-  ...(basePath && {
-    basePath,
-    assetPrefix: basePath,
-  }),
-
-  ...(process.env.OUTPUT_MODE === "export" && {
-    images: {
-      unoptimized: true,
-    },
-  }),
-  allowedDevOrigins: ["*"],
-
-  devIndicators: false,
-  poweredByHeader: false,
   reactStrictMode: true,
-
+  poweredByHeader: false,
   typescript: {
     ignoreBuildErrors: true,
   },
