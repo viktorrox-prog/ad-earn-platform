9058e7eeeb134e58194a2f6ee90dfda3ba16249e
viktorrox-prog <viktor.rox@yandex.ru>
2026-08-09 15:24:44 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   typescript: {
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    optimizeCss: false,
   },
 };
 
