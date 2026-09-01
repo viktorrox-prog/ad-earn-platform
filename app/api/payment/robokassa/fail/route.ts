62f03dd77ecf6434010dd027a85b9dc53d6d195f
viktorrox-prog <viktor.rox@yandex.ru>
2026-09-01 16:31:59 +0300

    Delete app/api/payment/robokassa directory

--- app/api/payment/robokassa/fail/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const baseUrl = request.nextUrl.origin;
-  return NextResponse.redirect(
-    new URL("/finance?error=payment_failed", baseUrl)
-  );
-}
